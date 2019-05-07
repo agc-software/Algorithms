3b7a6f1eaa7089866681916c3107ae7be8ff7cc8
Alex <agrahnchen.itprog@gmail.com>
2019-05-07 17:10:42 +0200

    More questions

---
+++ DailyCodingProblem/ProblemTen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.*/
+
+namespace Algorithms.DailyCodingProblem
+{
+    class ProblemTen
+    {
+    }
+}
